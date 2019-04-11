849929bcfe4bbfca4fbf42a39fa98589887fb50a
wangshuhao <276254221@qq.com>
2019-04-11 19:50:25 +0800

    文件反转功能

--- logger/file.go
+++ logger/file.go
@@ -1,24 +1,65 @@
 package logger
 
 import (
+	"bytes"
 	"github.com/youpenglai/goutils/pathtool"
 	"os"
 	"sync"
+	"time"
 )
 
 type FileWriter struct {
 	mu       sync.Mutex
 	filename string
 
-	rotate   bool
-	maxSize  int64
-	maxLines int
-	file     *os.File
-	wb       int64
+	logStartTime time.Time
+	//logCurTime   time.Time
+	currentFile  int
+	rotate       int
+	maxSize      int64
+	maxLines     int
+	maxFiles     int
+	file         *os.File
+	writeBytes   int64
+	writeLines   int
 }
 
-func (fw *FileWriter) openLogFile() {
-	pathtool.FileExists(fw.filename)
+func (fw *FileWriter) openLogFile() error {
+	exists, err := pathtool.FileExists(fw.filename)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (fw *FileWriter) needRotate() bool {
+	if fw.rotate == LogRotateNo {
+		return false
+	}
+
+	if fw.rotate == LogRotateDay {
+		now := time.Now()
+		if now.After(fw.logStartTime.Add(24 * time.Hour)) {
+			return true
+		}
+	} else if fw.rotate == LogRotateHour {
+		now := time.Now()
+		if now.After(fw.logStartTime.Add(time.Hour)) {
+			return true
+		}
+	} else if fw.rotate == LogRotateLines {
+		if fw.writeLines > fw.maxLines {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (fw *FileWriter) doRotate() error {
+
+	return nil
 }
 
 func (fw *FileWriter) Colorful() bool {
@@ -26,7 +67,21 @@ func (fw *FileWriter) Colorful() bool {
 }
 
 func (fw *FileWriter) Write(b []byte) (int, error) {
-	return 0, nil
+	fw.mu.Lock()
+	defer fw.mu.Unlock()
+
+	if fw.needRotate() {
+		fw.doRotate()
+	}
+
+	wn, err := fw.file.Write(b)
+	if err != nil {
+		return 0, err
+	}
+	fw.writeBytes += int64(wn)
+	fw.writeLines += bytes.Count(b, []byte{'\n'})
+
+	return wn, err
 }
 
 func (fw *FileWriter) Flush() {
@@ -36,6 +91,12 @@ func (fw *FileWriter) Flush() {
 }
 
 func NewFileWriter(opts LoggerOpts) LoggerWriter {
+	fw := &FileWriter{
+		rotate:   opts.Rotate,
+		filename: opts.FileName,
+		maxSize:  opts.MaxSize,
+		maxLines: opts.MaxLines,
+	}
 	return nil
 }
 
